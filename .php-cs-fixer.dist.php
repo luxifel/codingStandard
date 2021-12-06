f769c897fd810bd5ef16e1d61b25a2868bc95cf4
Nicola Olivetto <nicola.olivetto@drexcode.com>
2021-12-06 19:24:14 +0100

    Update libraries

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,27 +1,30 @@
 <?php
 
-$finder = PhpCsFixer\Finder::create()
-    ->exclude('_additional_structure')
-    ->name('*.php')
-    ->name('*.phtml');
+declare(strict_types=1);
 
-return PhpCsFixer\Config::create()
+$finder = PhpCsFixer\Finder::create()
+    ->name('*.php');
+
+$config = new PhpCsFixer\Config();
+$config
     ->setRules([
         '@PSR2' => true,
+        'array_indentation' => true,
         'array_syntax' => ['syntax' => 'short'],
+        'return_assignment' => true,
+        'align_multiline_comment' => ['comment_type' => 'phpdocs_like'],
         'blank_line_after_opening_tag' => true,
-        'blank_line_before_return' => true,
+        'blank_line_before_statement' => true,
         'cast_spaces' => true,
-        'class_definition' => ['multiLineExtendsEachSingleLine' => true],
+        'phpdoc_separation' => true,
+        'class_definition' => ['multi_line_extends_each_single_line' => true],
         'concat_space' => ['spacing' => 'one'],
         'declare_equal_normalize' => ['space' => 'single'],
         'function_typehint_space' => true,
-        'hash_to_slash_comment' => true,
-        'method_separation' => true,
         'no_blank_lines_after_phpdoc' => true,
         'no_blank_lines_after_class_opening' => true,
         'no_whitespace_in_blank_line' => true,
-        'no_short_echo_tag' => true,
+        'echo_tag_syntax' => ['format' => 'short'],
         'no_useless_else' => true,
         'no_empty_comment' => true,
         'no_empty_phpdoc' => true,
@@ -41,7 +44,10 @@ return PhpCsFixer\Config::create()
         'standardize_not_equals' => true,
         'ternary_operator_spaces' => true,
         'ternary_to_null_coalescing' => true,
-        'trailing_comma_in_multiline_array' => true,
+        'trailing_comma_in_multiline' => true,
         'whitespace_after_comma_in_array' => true,
     ])
+    ->setLineEnding("\r\n")
     ->setFinder($finder);
+
+return $config;
--- Makefile
+++ Makefile
@@ -4,20 +4,21 @@ list:
 	vendor/bin/phing -l
 
 #Perform syntax check and mess detection of sourcecode files, find duplicate code
-build:
-	    vendor/bin/phing build
+start:
+	    vendor/bin/phing start
 
 #Code style fixes - find coding standard violations and print human readable output
-style:
-	vendor/bin/phing style
+fix:
+	vendor/bin/phing fix
+
+dryrun:
+	vendor/bin/phing phpcsfixer-dry-run
 
 concat:
 	gulp concat
 
 purge:
 	gulp purge
-
-
 
 phing:
 	   echo vendor/bin/phing -Dmodule=path/custom/module
